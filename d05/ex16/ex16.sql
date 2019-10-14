ca65d925255345e265169fe45e34c52e9f62f335
JustQu <cool.3meu@gmail.com>
2019-10-14 20:47:01 +0300

    it works on my computer

---
+++ d05/ex16/ex16.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(date_last_film) AS movies
+FROM member
+WHERE date_last_film >= '2006-04-30' AND date_last_film <= '2007-07-30'
+	OR (DAYOFMONTH(date_last_film) = 24 AND MONTH(date_last_film) = 12);

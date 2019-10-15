b164fe684a4ca4a29ca85ad192e88a676e7e6fcf
JustQu <cool.3meu@gmail.com>
2019-10-15 11:45:06 +0300

    Update ex16.sql
    Possible fix

--- d05/ex16/ex16.sql
+++ d05/ex16/ex16.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(DISTINCT date_last_film) AS movies
+SELECT COUNT(DISTINCT id_film) AS movies
 FROM member
 WHERE date_last_film >= '2006-04-30' AND date_last_film <= '2007-07-30'
 	OR (DAYOFMONTH(date_last_film) = 24 AND MONTH(date_last_film) = 12);
